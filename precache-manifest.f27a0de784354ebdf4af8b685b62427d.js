5ba414a961df7908398ed8e0750abc76cd18bb48
Israel <israelthebest99@gmail.com>
2020-10-22 22:58:55 -0500

    Updates

---
+++ precache-manifest.f27a0de784354ebdf4af8b685b62427d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6373579e8b1bb37cd91bc9a11b8a02ea",
+    "url": "/mercadolibreapp/index.html"
+  },
+  {
+    "revision": "60f99ab3f0a534c2d832",
+    "url": "/mercadolibreapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0cb63da6e2a5f8bba083",
+    "url": "/mercadolibreapp/static/js/2.18f42fc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mercadolibreapp/static/js/2.18f42fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f99ab3f0a534c2d832",
+    "url": "/mercadolibreapp/static/js/main.3478840e.chunk.js"
+  },
+  {
+    "revision": "2616c88fcb93c9b37605",
+    "url": "/mercadolibreapp/static/js/runtime-main.417eb31b.js"
+  }
+]);
